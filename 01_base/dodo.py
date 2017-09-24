0671ce3eaa37f464cd86f7331c13518f6c44a68b
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2017-09-25 00:12:14 +0200

    Fixed docstrings for 01_base/dodo.py
    	modified:   01_base/dodo.py

--- 01_base/dodo.py
+++ 01_base/dodo.py
@@ -38,10 +38,12 @@ class TaskContext(object):
         """path to json file with alldata information"""
 
     def ensure_target_dir(self):
+        """Ensure the target dir exists, create it, if missing"""
         self.target_dir.mkdir(parents=True, exist_ok=True)
         return True
 
     def remove_target_dir(self):
+        """Remove the target dir. Will fail if not empty."""
         if self.target_dir.exists():
             self.target_dir.rmdir()
         return True
